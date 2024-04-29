80140e5b91d39ede68bed9283604f4e24888855b
Javier Diaz <88525089+Javi-Diaz@users.noreply.github.com>
2024-04-29 13:11:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,52 @@
-let menuBtn = document.querySelector(".header-nav-btnMenu")
-let menu = document.querySelector(".header-nav-ul")
+/* EFECTO ACTIVE */
+/* let links = document.querySelectorAll(".header-nav-ul-li-a")
 
-menuBtn.addEventListener("click",()=>{
-    menu.classList.toggle("header-nav-ulOpen")
-})
+links.forEach(link=>{
+    link.addEventListener("click",()=>{
+        document.querySelector(".header-nav-ul-li-a-active").classList.remove(".header-nav-ul-li-a-active")
+        link.classList.add("header-nav-ul-li-a-active")
+    })  
+}) */
 
 
 
+/* PROPAGANDA CELULAR*/
+/* let body = document.querySelector('body');
+let slide = document.querySelector('#caja-ropa');
+let widthWindow = body.clientWidth;
+let widthBox = slide.clientWidth;
+let widDif = widthWindow - widthBox;
+let width = slide.clientWidth + widDif;
+let slider = document.querySelector('#slider-propaganda').animate([
+   // keyframes
+   { transform: 'translateX(0px)' },
+   { transform: 'translateX(' + -(width * 2) + 'px)' }
+ ], {
+   // timing options
+   duration: 4000,
+   iterations: Infinity
+ }); */
 
+const slider = document.querySelector("#slider")
+let sliderSection = document.querySelectorAll(".slider-section")
+let sliderSectionLast = sliderSection[sliderSection.length-1]
 
+slider.insertAdjacentElement("afterbegin",sliderSectionLast)
+
+function next(){
+  let sliderSectionFirst = document.querySelectorAll(".slider-section")[0]
+  slider.style.marginLeft = "-200%"
+  slider.style.transition = "all .5s"
+  setTimeout(function(){
+    slider.style.transition = "none"
+    slider.insertAdjacentElement("beforeend",sliderSectionFirst)
+    slider.style.marginLeft = "-100%"
+  },500)
+}
+
+setInterval(function(){
+  next()
+},3000)
 
 
 
